e4859b7ab384681619f185ff9828023cead48437
helloyou2012 <helloyou2012@yeah.net>
2020-07-17 17:31:33 +0800

    add favicon

--- clean.js
+++ clean.js
@@ -2,4 +2,5 @@ const fs = require('fs-extra');
 
 fs.emptyDirSync('dist');
 fs.copySync('index.html', 'dist/index.html');
+fs.copySync('favicon.ico', 'dist/favicon.ico');
 fs.copySync('fonts', 'dist/fonts');
